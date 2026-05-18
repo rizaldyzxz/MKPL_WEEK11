f2ab5787dacdda6025744320d30d6912bf839bd6
Rizaldy Aulia Rachman <aldyauliarahman16@gmail.com>
2026-05-18 22:07:26 +0700

    test

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -1,7 +1,4 @@
-package src.test.java;
-
 import static org.junit.Assert.*;
-import src.main.java.*;
 import org.junit.Test;
 
 public class CounterTest {
@@ -9,21 +6,21 @@ public class CounterTest {
 	@Test
 	public void testReset() {
 		Counter testCounter = new Counter();
-		
-		for(int i = 0; i <10; i++) {
+
+		for (int i = 0; i < 10; i++) {
 			testCounter.increment();
 		}
-		
+
 		testCounter.reset();
-		
+
 		assertEquals(testCounter.getCount(), 0);
 	}
 
 	@Test
 	public void testIncrement() {
 		Counter testCounter = new Counter();
-		
-		for(int i = 1; i <10; i++) {
+
+		for (int i = 1; i < 10; i++) {
 			testCounter.increment();
 			assertEquals(testCounter.getCount(), i);
 		}
@@ -32,8 +29,8 @@ public class CounterTest {
 	@Test
 	public void testDecrement() {
 		Counter testCounter = new Counter();
-		
-		for(int i = 1; i <10; i++) {
+
+		for (int i = 1; i < 10; i++) {
 			testCounter.decrement();
 			assertEquals(testCounter.getCount(), i * -1);
 		}
